a249bddd5daf0623e38825390e6356deeff60968
fingerissue <fingerissue@gmail.com>
2026-09-04 18:12:46 +0900

    [Passed] 입력받아 계산 4 (2ms, 0MB)

---
+++ trail1/입력받아 계산 4/input-calculate-4.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).expect("입력 실패");
+    let mut a : i16 = a.trim().parse().expect("숫자가 아니거나 너무 큼");
+
+    a *= 2;
+    
+    println!("{}", a);
+}
