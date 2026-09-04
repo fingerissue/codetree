a4b812ce3cda81b4eaa5f9934e759a1200615d74
fingerissue <fingerissue@gmail.com>
2026-09-04 22:28:37 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail1/세 실수의 반올림/rounding-of-three-actual-numbers.rs
@@ -0,0 +1,16 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).expect("입력 실패");
+    let mut buffer = buffer.split_whitespace();
+
+    let a : f32 = buffer.next().expect("첫 번째 입력값 없음")
+                        .parse().expect("숫자가 아니거나 값이 너무 큼");
+    let b : f32 = buffer.next().expect("두 번째 입력값 없음")
+                        .parse().expect("숫자가 아니거나 값이 너무 큼");
+    let c : f32 = buffer.next().expect("세 번째 입력값 없음")
+                        .parse().expect("숫자가 아니거나 값이 너무 큼");
+    
+    println!("{:.3}\n{:.3}\n{:.3}", a, b, c);
+}
