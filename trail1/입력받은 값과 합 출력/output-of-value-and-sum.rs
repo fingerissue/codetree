9a1bbcfe30fff124fe88fc3887ab894c5832ed62
fingerissue <fingerissue@gmail.com>
2026-09-04 19:28:43 +0900

    [Passed] 입력받은 값과 합 출력 (2ms, 0MB)

---
+++ trail1/입력받은 값과 합 출력/output-of-value-and-sum.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("입력 불가");
+    let mut input = input.split_whitespace();
+
+    let a : i16 = input.next().expect("첫 번째 값이 없음")
+                    .parse().expect("정수가 아니거나 값이 큼");
+    let b : i16 = input.next().expect("두 번째 값이 없음")
+                    .parse().expect("정수가 아니거나 값이 큼");
+
+    println!("{} {} {}", a, b, a + b);
+}
