7b1abd1e4e4f3e01973893dd1d35247c61a44acf
fingerissue <fingerissue@gmail.com>
2026-09-05 13:47:20 +0900

    [Passed] 합과 평균 (2ms, 0MB)

---
+++ trail1/합과 평균/sum-and-avg.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("입력 불가");
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let a : i16 = tokens.next().expect("첫 번째 입력값이 없음")
+                        .parse().expect("숫자가 아니거나 너무 큼");
+    let b : i16 = tokens.next().expect("두 번째 입력값이 없음")
+                        .parse().expect("숫자가 아니거나 너무 큼");
+                                                                                                                                            
+    println!("{} {:.1}", a + b, (a + b) as f32 / 2.0);
+}
