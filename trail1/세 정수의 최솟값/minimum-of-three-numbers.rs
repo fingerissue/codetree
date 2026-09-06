db0f5c46aa1420f2af410b8167b6393a70ba1181
fingerissue <fingerissue@gmail.com>
2026-09-06 11:57:20 +0900

    [Passed] 세 정수의 최솟값 (2ms, 0MB)

---
+++ trail1/세 정수의 최솟값/minimum-of-three-numbers.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let a : i8 = tokens.next().unwrap().parse().unwrap();
+    let b : i8 = tokens.next().unwrap().parse().unwrap();
+    let c : i8 = tokens.next().unwrap().parse().unwrap();
+
+    if a <= b && a <= c {
+        println!("{}", a);
+    } else if b <= a && b <= c {
+        println!("{}", b);
+    } else {
+        println!("{}", c);
+    }
+}
