ff4517213fbb0e5ad24e206f4574e4c0e30f51b3
fingerissue <fingerissue@gmail.com>
2026-09-15 16:42:16 +0900

    [Passed] 홀수만 출력하기 (3ms, 0MB)

---
+++ trail1/홀수만 출력하기/print-only-odd-numbers.rs
@@ -0,0 +1,18 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    for token in tokens {
+        let token : i8 = token.parse().unwrap();
+        if token % 2 == 1 && token % 3 == 0 {
+            println!("{}", token);
+        }
+    }
+}
