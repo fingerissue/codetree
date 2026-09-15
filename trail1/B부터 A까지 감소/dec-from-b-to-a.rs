aab5a0ac6c7fc4a87a99726c8e9d6ab9db3f2b90
fingerissue <fingerissue@gmail.com>
2026-09-15 14:50:15 +0900

    [Passed] B부터 A까지 감소 (2ms, 0MB)

---
+++ trail1/B부터 A까지 감소/dec-from-b-to-a.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let a : i8 = tokens.next().unwrap().parse().unwrap();
+    let b : i8 = tokens.next().unwrap().parse().unwrap();
+
+    for i in (a..=b).rev() {
+        print!("{} ", i);
+    }
+}
