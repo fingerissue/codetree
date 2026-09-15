411305fdc05242e650537ac07573cb751fd28c8c
fingerissue <fingerissue@gmail.com>
2026-09-15 15:08:42 +0900

    [Passed] 짝수만 출력 (3ms, 0MB)

---
+++ trail1/짝수만 출력/output-only-even.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let mut a : i8 = tokens.next().unwrap().parse().unwrap();
+    let b : i8 = tokens.next().unwrap().parse().unwrap();
+    
+    while a <= b {
+        print!("{} ", a);
+        a += 2;
+    }
+}
