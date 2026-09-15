87f63504bc0e30f615e95ad9898e24968ba8f985
fingerissue <fingerissue@gmail.com>
2026-09-15 14:51:33 +0900

    [Passed] 홀수만 출력 2 (2ms, 0MB)

---
+++ trail1/홀수만 출력 2/output-only-odd-2.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let a : i8 = tokens.next().unwrap().parse().unwrap();
+    let b : i8 = tokens.next().unwrap().parse().unwrap();
+
+    for i in (b..=a).rev() {
+        if i % 2 == 1 {
+            print!("{} ", i);
+        }
+    }
+}
