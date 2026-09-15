fcdf5f8e38bac91f44fc44f8223417341ef39771
fingerissue <fingerissue@gmail.com>
2026-09-15 14:28:23 +0900

    [Passed] 입력받는 수 부터 100까지 출력 (2ms, 0MB)

---
+++ trail1/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for i in n..=100 {
+        print!("{} ", i);
+    }
+}
