4a647ab9efadfe56d6bce35aa28c6c44fe7838f8
fingerissue <fingerissue@gmail.com>
2026-09-15 14:48:50 +0900

    [Passed] 특정 숫자 도달하기 2 (2ms, 0MB)

---
+++ trail1/특정 숫자 도달하기 2/reaching-specific-number-2.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for i in (1..=n).rev() {
+        print!("{} ", i);
+    }
+}
