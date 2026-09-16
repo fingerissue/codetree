30778c3ed4a1498d960c83073e6f43ea250a878c
fingerissue <fingerissue@gmail.com>
2026-09-17 00:19:30 +0900

    [Passed] ab 사이에 없는 c (2ms, 0MB)

---
+++ trail1/ab 사이에 없는 c/c-between-a-and-b-2.rs
@@ -0,0 +1,21 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let a : i8 = tokens.next().unwrap().parse().unwrap();
+    let b : i8 = tokens.next().unwrap().parse().unwrap();
+    let c : i8 = tokens.next().unwrap().parse().unwrap();
+
+    let mut satisfied = true;
+
+    for i in a..=b {
+        if i % c == 0 {
+            satisfied = false;
+        }
+    }
+
+    println!("{}", if satisfied { "YES" } else { "NO" });
+}
