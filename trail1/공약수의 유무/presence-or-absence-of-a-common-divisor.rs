0254ed3f5ccd4fe75cee5075965edab800e75e65
fingerissue <fingerissue@gmail.com>
2026-09-17 00:12:22 +0900

    [Passed] 공약수의 유무 (2ms, 0MB)

---
+++ trail1/공약수의 유무/presence-or-absence-of-a-common-divisor.rs
@@ -0,0 +1,20 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let a : i16 = tokens.next().unwrap().parse().unwrap();
+    let b : i16 = tokens.next().unwrap().parse().unwrap();
+
+    let mut is_gongyaksu = false;
+
+    for i in a..=b {
+        if 1920 % i == 0 && 2880 % i == 0 {
+            is_gongyaksu = true;
+        }
+    }
+
+    println!("{}", is_gongyaksu as u8);
+}
