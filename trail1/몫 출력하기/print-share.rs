b66d970268b5164e347318d246427b557e260f0c
fingerissue <fingerissue@gmail.com>
2026-09-16 23:19:58 +0900

    [Passed] 몫 출력하기 (2ms, 0MB)

--- trail1/몫 출력하기/print-share.rs
+++ trail1/몫 출력하기/print-share.rs
@@ -8,9 +8,6 @@ fn main() {
         let n : i8 = buffer.trim().parse().unwrap();
 
         if n % 2 == 0 {
-            if cnt == 3 {
-                break;
-            }
             println!("{}", n / 2);
             cnt += 1;
         }
