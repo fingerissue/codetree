c244238e51b50692c2711a739923f699422670d7
fingerissue <fingerissue@gmail.com>
2026-09-16 17:43:23 +0900

    [Passed] 등장횟수 확인 (2ms, 0MB)

---
+++ trail1/등장횟수 확인/check-number-of-appearances.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let mut cnt : i8 = 0;
+    
+    for token in tokens {
+        let token : i16 = token.parse().unwrap();
+        
+        if token % 2 == 0 {
+            cnt += 1;
+        }
+    }
+
+    println!("{}", cnt);
+}
