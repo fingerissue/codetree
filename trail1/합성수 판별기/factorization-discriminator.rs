63bc02ba9f537f482ad2020cfa30247118b12824
fingerissue <fingerissue@gmail.com>
2026-09-17 00:09:00 +0900

    [Passed] 합성수 판별기 (2ms, 0MB)

---
+++ trail1/합성수 판별기/factorization-discriminator.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i16 = buffer.trim().parse().unwrap();
+
+    let mut is_hapsungsu = false;
+
+    for i in 2..n {
+        if n % i == 0 {
+            is_hapsungsu = true;
+        }
+    }
+
+    println!("{}", if is_hapsungsu { "C" } else { "N" });
+}
