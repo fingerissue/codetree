3d84939ab48db20193edb60186e60f2447ec5dc3
fingerissue <fingerissue@gmail.com>
2026-09-17 12:27:01 +0900

    [Passed] 별 그리기 (2ms, 0MB)

---
+++ trail1/별 그리기/star-drawing.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i16 = buffer.trim().parse().unwrap();
+
+    for i in 0..n {
+        for _ in 0..n - i - 1 {
+            print!(" ");
+        }
+        for _ in 0..2 * i + 1 {
+            print!("*");
+        }
+        print!("\n");
+    }
+    for i in 0..n - 1 {
+        for _ in 0..i + 1 {
+            print!(" ");
+        }
+        for _ in 0..2 * n - 2 * i - 3 {
+            print!("*");
+        }
+        print!("\n");
+    }
+}
