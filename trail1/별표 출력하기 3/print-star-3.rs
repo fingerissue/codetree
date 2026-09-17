7d1c38c9261df052c200a034d524982aefef411d
fingerissue <fingerissue@gmail.com>
2026-09-17 11:59:47 +0900

    [Passed] 별표 출력하기 3 (2ms, 0MB)

---
+++ trail1/별표 출력하기 3/print-star-3.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for i in 0..n {
+        for _ in 0..i {
+            print!("  ");
+        }
+        
+        for _ in (0..2 * n - 2 * i - 1).rev() {
+            print!("* ");
+        }
+        print!("\n");
+    }
+}
