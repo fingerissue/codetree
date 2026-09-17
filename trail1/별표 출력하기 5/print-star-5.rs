f2aca40a569e13277a87259b1360a76da7175b23
fingerissue <fingerissue@gmail.com>
2026-09-17 11:26:44 +0900

    [Passed] 별표 출력하기 5 (3ms, 0MB)

---
+++ trail1/별표 출력하기 5/print-star-5.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for i in (0..n).rev() {
+        for _ in 0..=i {
+            for _ in 0..=i {
+                print!("*");
+            }
+            print!(" ");
+        }
+        print!("\n");
+    }
+}
