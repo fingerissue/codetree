f33622fa4aad88b53d19eaba847996ec10f937bc
fingerissue <fingerissue@gmail.com>
2026-09-18 15:51:38 +0900

    [Passed] 행 단위로 반복하여 출력 (2ms, 0MB)

---
+++ trail1/행 단위로 반복하여 출력/print-in-row.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for _ in 1..=n {
+        for i in 1..=n {
+            print!("{}", i);
+        }
+        print!("\n");
+    }
+}
