92beb6f049615d254b94037f90b47f7a67e47144
fingerissue <fingerissue@gmail.com>
2026-09-18 15:57:51 +0900

    [Passed] 순서쌍 만들기 (2ms, 0MB)

---
+++ trail1/순서쌍 만들기/making-order-pair.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for i in (1..=n).rev() {
+        for j in (1..=n).rev() {
+            print!("({},{}) ", i, j);
+        }
+        print!("\n");
+    }
+}
