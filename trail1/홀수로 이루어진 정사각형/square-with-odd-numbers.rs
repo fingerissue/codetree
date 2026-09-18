806c10cc7c37dc176381e1e9597d6e18ab4e7523
fingerissue <fingerissue@gmail.com>
2026-09-18 16:09:42 +0900

    [Passed] 홀수로 이루어진 정사각형 (2ms, 0MB)

---
+++ trail1/홀수로 이루어진 정사각형/square-with-odd-numbers.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for i in 0..n {
+        for j in 0..n {
+            print!("{} ", 11 + (i + j) * 2);
+        }
+        print!("\n");
+    }
+}
