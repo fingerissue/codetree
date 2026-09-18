1540bbad223602b895e635d5e47913cd21e2cbc5
fingerissue <fingerissue@gmail.com>
2026-09-18 16:18:30 +0900

    [Passed] 1-9 정사각형 (2ms, 0MB)

---
+++ trail1/1-9 정사각형/1-9-square.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    let mut cnt = 1;
+
+    for _ in 0..n {
+        for _ in 0..n {
+            print!("{}", cnt);
+
+            if cnt == 9 {
+                cnt = 1;
+            } else {
+                cnt += 1;
+            }
+        }
+        print!("\n");
+    }
+}
