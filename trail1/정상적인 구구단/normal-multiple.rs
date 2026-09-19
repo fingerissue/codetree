eb826ab916d07938d76cb04a5c5159c9f5501f70
fingerissue <fingerissue@gmail.com>
2026-09-19 12:03:21 +0900

    [Passed] 정상적인 구구단 (2ms, 0MB)

---
+++ trail1/정상적인 구구단/normal-multiple.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for i in 1..=n {
+        for j in 1..=n {
+            print!("{} * {} = {}", i, j, i * j);
+            if j < n {
+                print!(", ");
+            }
+        }
+        print!("\n");
+    }
+}
