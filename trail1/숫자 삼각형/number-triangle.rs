faa84169e3a32e77ac0c592c699d4a8ac90e45cf
fingerissue <fingerissue@gmail.com>
2026-09-19 12:16:13 +0900

    [Passed] 숫자 삼각형 (2ms, 0MB)

---
+++ trail1/숫자 삼각형/number-triangle.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let n : i8 = buffer.trim().parse().unwrap();
+
+    for i in 0..n {
+        for j in 0..=i {
+            if i >= j {
+                print!("{} ", j + 1);
+            }
+        }
+        print!("\n");
+    }
+}
