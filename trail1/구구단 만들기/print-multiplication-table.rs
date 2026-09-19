3d7fc03528e2a935ba3f434f46a29919d287ec06
fingerissue <fingerissue@gmail.com>
2026-09-19 12:07:24 +0900

    [Passed] 구구단 만들기 (2ms, 0MB)

---
+++ trail1/구구단 만들기/print-multiplication-table.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let mut tokens = buffer.trim().split_whitespace();
+
+    let a : i8 = tokens.next().unwrap().parse().unwrap();
+    let b : i8 = tokens.next().unwrap().parse().unwrap();
+
+    for i in 1..10 {
+        for j in (a.min(b)..=b.max(a)).rev() {
+            if j % 2 == 1 {
+                continue;
+            }
+
+            print!("{} * {} = {}", j, i, i * j);
+            if j > a.min(b) {
+                print!(" / ");
+            }
+        }
+        print!("\n");
+    }
+}
