3f1408a6ca834db7d0f944ab3b325dda731474ca
fingerissue <fingerissue@gmail.com>
2026-09-19 23:01:18 +0900

    [Passed] 짝수의 합 n번 구하기 (2ms, 0MB)

--- trail1/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.rs
+++ trail1/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.rs
@@ -5,7 +5,7 @@ fn main() {
     io::stdin().read_line(&mut buffer).unwrap();
     let n : i8 = buffer.trim().parse().unwrap();
 
-    let mut sum : i16 = 1;
+    let mut sum : i16 = 0;
 
     for i in 0..n {
         buffer.clear();
@@ -16,9 +16,9 @@ fn main() {
         let b : i8 = tokens.next().unwrap().parse().unwrap();
         sum = 0;
 
-        for i in a..=b {
-            if i % 2 == 0 {
-                sum += i as i16;
+        for j in a..=b {
+            if j % 2 == 0 {
+                sum += j as i16;
             }
         }
         println!("{}", sum);
